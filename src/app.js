a9633f70330cecccd7a725fff934d876e65a56df
Shikhar <shikharsr100@gmail.com>
2022-01-18 19:47:27 +0530

    Removed dummy expenses

--- src/app.js
+++ src/app.js
@@ -11,9 +11,6 @@ import { addExpense } from './actions/expenses';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 5000 }));
-store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }));
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
